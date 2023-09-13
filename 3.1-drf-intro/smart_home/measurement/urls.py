38b2a19b4058d15d7b29b1e6faa4a2e35f362183
Lina_Held_Void <iltep118666@gmail.com>
2023-09-13 12:57:55 +0400

    DRF. Попытка 1

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
+from .views import SensorCreateView, SensorUpdateView, MeasurementView, SensorDetailView
+
+
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorCreateView.as_view()),
+    path('sensors/<id>/', SensorUpdateView.as_view()),
+    path('measurements/', MeasurementView.as_view()),
+    path('sensor/<id>', SensorDetailView.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,47 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import \
+    ListCreateAPIView, \
+    RetrieveUpdateAPIView, \
+    CreateAPIView
+from rest_framework.response import Response
+
+from .models import Sensor, Measurement
+from .serializers import \
+    SensorSerializer, \
+    SensorDetailSerializer, \
+    MeasurementDetailSerializer, \
+    MeasurementSerializer
+
+
+class SensorCreateView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorUpdateView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+    lookup_field = 'id'
+
+
+class MeasurementView(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementDetailSerializer
+
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+    lookup_field = 'id'
+
+    def get(self, request, *args, **kwargs):
+        sensor = self.get_object()
+        serializer = self.get_serializer(sensor)
+        data = serializer.data
+
+        measurement = Measurement.objects.filter(sensor=sensor)
+        measurement_serializer = MeasurementSerializer(measurement, many=True)
+        data['measurements'] = measurement_serializer.data
+
+        return Response(data)
